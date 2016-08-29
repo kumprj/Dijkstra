e88cd30103d4817a499a6bc7a3e82e86f3568619
Robert Kump <kumprj@miamioh.edu>
2016-08-29 12:19:09 -0400

    Fixed a comment.

--- src/Tester.java
+++ src/Tester.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Tester {
 	
 	/*
-	 * Main method that handles the console interface
+	 * Main method that handles the console interface.
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
